9023a1b0bbb328f99f166a2bdb7f05e88139785b
PabloCarrai <pablohernanjuan@hotmail.com>
2025-03-02 20:18:08 -0300

    Agregando ejercicios

--- DOM-Css/programa251.js
+++ DOM-Css/programa251.js
@@ -4,3 +4,23 @@ Mostrar una imagen y mediante 3 botones permitir
 aumentar o reducir el valor de la propiedad border-radius de
 su estilo
 */
+
+// obtengo referencia de la foto
+const foto1 = document.querySelector("#foto1")
+
+document.querySelector("#reducir").addEventListener("click", () => {
+    // obtengo el radio actual y lo guardo
+    let radio = parseInt(foto1.style.borderRadius)
+    if (radio > 0) {
+        radio -= 5
+        foto1.style.borderRadius = radio + "%"
+    }
+})
+document.querySelector("#aumentar").addEventListener("click", () => {
+    // obtengo el radio actual y lo guardo
+    let radio = parseInt(foto1.style.borderRadius)
+    if (radio < 50) {
+        radio += 5
+        foto1.style.borderRadius = radio + "%"
+    }
+})
