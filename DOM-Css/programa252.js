defd798e97b346d55618b62ba20d03262294b011
PabloCarrai <pablohernanjuan@hotmail.com>
2025-03-03 12:08:14 -0300

    Agregando ejercicios

---
+++ DOM-Css/programa252.js
@@ -0,0 +1,17 @@
+function actualizarPropiedades(ancho, alto, color) {
+    document.querySelector("#recuadro").style.width = `${ancho}px`
+    document.querySelector("#recuadro").style.height = `${alto}px`
+    document.querySelector("#recuadro").style.backgroundColor = `${color}`
+}
+
+
+document.querySelector("#actualizar").addEventListener("click", () => {
+    let ancho = parseInt(document.querySelector("#ancho").value)
+    let alto = parseInt(document.querySelector("#alto").value)
+    let color = document.querySelector("#color").value
+    actualizarPropiedades(ancho, alto, color)
+
+    //console.log(typeof ancho,typeof alto,typeof color)
+
+
+})
