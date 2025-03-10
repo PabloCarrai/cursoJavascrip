8ccbdeed24118b64c0774c97d9d1249b7a73e966
PabloCarrai <pablohernanjuan@hotmail.com>
2025-03-10 11:53:34 -0300

    Agregando ejercicios

--- DOM-Css/programa258.js
+++ DOM-Css/programa258.js
@@ -1,4 +1,18 @@
 let contador = 1
+deshabilitarBoton(contador)
+
+function deshabilitarBoton(contador) {
+    if (contador == 1) {
+        document.querySelector("#anterior").disabled = true
+    } else {
+        document.querySelector("#anterior").disabled = false
+    }
+    if (contador == 4) {
+        document.querySelector("#siguiente").disabled = true
+    } else {
+        document.querySelector("#siguiente").disabled = false
+    }
+}
 
 function cambiarImagen(operacion) {
     let imagen = `file:///home/ed/JavasCript/DOM-Css/imagenes/avatar${contador}.png`
@@ -8,35 +22,21 @@ function cambiarImagen(operacion) {
     if (operacion == "resta") {
         contador--
     }
+    if (contador < 1) {
+        contador = 1
+    }
+    if (contador > 4) {
+        contador = 4
+    }
     return imagen
 }
 
-
-
 document.querySelector("#siguiente").addEventListener("click", () => {
-    let estado = false
-    if (contador > 4) {
-        contador = 4
-        document.querySelector("#siguiente").disabled = estado
-    } else {
-        document.querySelector("#siguiente").disabled = estado
-    }
-    console.log(contador)
+    deshabilitarBoton(contador)
     document.querySelector("#imagen").src = cambiarImagen("suma")
-
 })
 
 document.querySelector("#anterior").addEventListener("click", () => {
-    let estado = true
-    if (contador < 1) {
-        contador = 1
-        document.querySelector("#anterior").disabled = estado
-    } else {
-        if (contador != 1) {
-            estado = true
-            document.querySelector("#anterior").disabled = estado
-        }
-    }
-    console.log(contador)
+    deshabilitarBoton(contador)
     document.querySelector("#imagen").src = cambiarImagen("resta")
 })
