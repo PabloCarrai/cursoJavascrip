d84374122221f071543f19c7d01813d0293ee141
PabloCarrai <pablohernanjuan@hotmail.com>
2025-03-13 11:55:48 -0300

    Agregando ejercicios

---
+++ DOM-Css/programa267.js
@@ -0,0 +1,23 @@
+function adivinarAleatorio(numero) {
+    let aleatorio = Math.floor(Math.random() * 101)
+    console.log(aleatorio)
+    if (aleatorio == numero) {
+        return `Ganastes, muy bien el numero que pense es el ${aleatorio}`
+    } else {
+        if (aleatorio < numero) {
+            return `El numero que pense es menor a ${numero}`
+        } else {
+            if (aleatorio > numero) {
+                return `El numero que pense es mayor a ${numero}`
+            }
+        }
+    }
+
+}
+document.querySelector("#verificar").addEventListener("click", () => {
+    const valor = document.querySelector("#numero").value
+    let listaExistente = document.querySelector("#intentos")
+    let liitem = document.createElement("li")
+    liitem.innerHTML = adivinarAleatorio(valor)
+    listaExistente.append(liitem)
+})
