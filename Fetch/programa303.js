c68bed0cdc12513c5b72e50fd09ce7a608e3cb51
PabloCarrai <pablohernanjuan@hotmail.com>
2025-04-26 11:56:24 -0300

    Tengo que volver a ver el video, hace bocha que lo deje pendiente y no me acuerdo bien como venia.

---
+++ Fetch/programa303.js
@@ -0,0 +1,33 @@
+
+const provincias = [
+    "Buenos Aires",
+    "Catamarca",
+    "Chaco",
+    "Chubut",
+    "Córdoba",
+    "Corrientes",
+    "Entre Ríos",
+    "Formosa",
+    "Jujuy",
+    "La Pampa",
+    "La Rioja",
+    "Mendoza",
+    "Misiones",
+    "Neuquén",
+    "Río Negro",
+    "Salta",
+    "San Juan",
+    "San Luis",
+    "Santa Cruz",
+    "Santa Fe",
+    "Santiago del Estero",
+    "Tierra del Fuego",
+    "Tucumán"
+]
+
+provincias.forEach( (prov) => {
+
+    const ul=document.querySelector("#provincias")
+    ul.innerHTML+=`<a href=""><li>${prov}</li>`
+    //console.log(`<li>${prov}</li>`)
+})
